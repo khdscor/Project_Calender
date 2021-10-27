244baadd335e7cfafe6b0e138a6a7eaf5d7a76a2
khdscor <khds_cor@naver.com>
2021-10-27 15:39:18 +0900

    feat: 기본 환경세팅 완료

---
+++ src/test/java/calender/calender/CalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package calender.calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
